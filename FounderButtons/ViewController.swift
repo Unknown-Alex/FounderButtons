0448849254abfc1cbb9f88341d1660043ee4a15d
Alex <amcclain1985@gmail.com>
2021-09-03 19:50:07 -0400

    Founder Button Task

--- FounderButtons/ViewController.swift
+++ FounderButtons/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var nameOneLabel: UILabel!
+    @IBOutlet weak var nameTwoLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func googleButton(_ sender: UIButton) {
+        print("Google button was Pressed!")
+        nameOneLabel.text = "Larry Page"
+        nameTwoLabel.text = "Sergey Brin"
+    }
+    
+    @IBAction func runwayButton(_ sender: UIButton) {
+        print("Runway button was Pressed!")
+        nameOneLabel.text = "Jennifer Hyman"
+        nameTwoLabel.text = "Jenny Fleiss"
+    }
+    
+    @IBAction func clearButton(_ sender: UIButton) {
+        print("Clear button was pressed!")
+        nameOneLabel.text = ""
+        nameTwoLabel.text = ""
+        
+    }
 }
 
